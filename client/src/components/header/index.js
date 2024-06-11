5c52224458aae01bbb14ca35d59ed84ca7187f6d
Justin Kwok <jkwok0714@gmail.com>
2024-06-11 00:07:38 -0700

    Upgrade preact

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -19,104 +19,131 @@ const smolThing = require('../../img/aguaPlus_smol.png');
 
 const DARKMODE_KEY = 'darkMode';
 export default class Header extends Component {
-	closeDrawer() {
-		this.drawer.MDComponent.open = false;
-		this.state = {
-			darkThemeEnabled: false
-		};
-	}
+  closeDrawer() {
+    this.drawer.MDComponent.open = false;
+    this.state = {
+      darkThemeEnabled: false,
+    };
+  }
 
-	openDrawer = () => (this.drawer.MDComponent.open = true);
+  openDrawer = () => (this.drawer.MDComponent.open = true);
 
-	openSettings = () => this.dialog.MDComponent.show();
+  openSettings = () => this.dialog.MDComponent.show();
 
-	drawerRef = drawer => (this.drawer = drawer);
-	dialogRef = dialog => (this.dialog = dialog);
+  drawerRef = (drawer) => (this.drawer = drawer);
+  dialogRef = (dialog) => (this.dialog = dialog);
 
-	linkTo = path => () => {
-		route(path);
-		this.closeDrawer();
-	};
+  linkTo = (path) => () => {
+    route(path);
+    this.closeDrawer();
+  };
 
-	goHome = this.linkTo('/');
-	goToMyProfile = this.linkTo('/profile');
-	goToUpload = this.linkTo('/upload');
-	goToBugs = this.linkTo('/bugs');
+  goHome = this.linkTo('/');
+  goToMyProfile = this.linkTo('/profile');
+  goToUpload = this.linkTo('/upload');
+  goToBugs = this.linkTo('/bugs');
 
-	toggleDarkTheme = () => {
-		this.setState(
-			{
-				darkThemeEnabled: !this.state.darkThemeEnabled
-			},
-			this.handleDarkTheme
-		);
-	}
+  toggleDarkTheme = () => {
+    this.setState(
+      {
+        darkThemeEnabled: !this.state.darkThemeEnabled,
+      },
+      this.handleDarkTheme
+    );
+  };
 
-	handleDarkTheme = () => {
-		if (this.state.darkThemeEnabled) {
-			document.body.classList.add('mdc-theme--dark');
-		} else {
-			document.body.classList.remove('mdc-theme--dark');
-		}
-		localStorage.setItem(DARKMODE_KEY, this.state.darkThemeEnabled);
-	};
+  handleDarkTheme = () => {
+    if (this.state.darkThemeEnabled) {
+      document.body.classList.add('mdc-theme--dark');
+    } else {
+      document.body.classList.remove('mdc-theme--dark');
+    }
+    localStorage.setItem(DARKMODE_KEY, this.state.darkThemeEnabled);
+  };
 
-	componentDidMount () {
-		const lDMode = localStorage.getItem(DARKMODE_KEY);
-		if (!!lDMode) {
-			this.setState({ darkThemeEnabled: lDMode === 'true' }, this.handleDarkTheme);
-		}
-	}
+  componentDidMount() {
+    const lDMode = localStorage.getItem(DARKMODE_KEY);
+    if (!!lDMode) {
+      this.setState(
+        { darkThemeEnabled: lDMode === 'true' },
+        this.handleDarkTheme
+      );
+    }
+  }
 
-	render(props) {
-		return (
-			<div>
-				<TopAppBar className="topappbar">
-					<TopAppBar.Row>
-						<TopAppBar.Section align-start>
-							<TopAppBar.Icon menu onClick={this.openDrawer}>
-								menu
-							</TopAppBar.Icon>
-							<TopAppBar.Title style={{ cursor: 'pointer' }} onClick={this.goHome}><img class={style.smolIcon} src={smolThing} />Agua+</TopAppBar.Title>
-						</TopAppBar.Section>
-						<TopAppBar.Section align-end shrink-to-fit>
-							<SpaceMeter />
-							<TopAppBar.Icon onClick={this.openSettings}>settings</TopAppBar.Icon>
-						</TopAppBar.Section>
-					</TopAppBar.Row>
-				</TopAppBar>
-				<Drawer modal ref={this.drawerRef}>
-					<Drawer.DrawerContent>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/'} onClick={this.goHome}>
-							<List.ItemGraphic>home</List.ItemGraphic>
-							Home
-						</Drawer.DrawerItem>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/upload'} onClick={this.goToUpload}>
-							<List.ItemGraphic>cloud_upload</List.ItemGraphic>
-							Upload
-						</Drawer.DrawerItem>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/bugs'} onClick={this.goToBugs}>
-							<List.ItemGraphic>bug_report</List.ItemGraphic>
-							Bug Reporting
-						</Drawer.DrawerItem>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/profile'} onClick={this.goToMyProfile}>
-							<List.ItemGraphic>account_circle</List.ItemGraphic>
-							Profile
-						</Drawer.DrawerItem>
-					</Drawer.DrawerContent>
-				</Drawer>
-				<Dialog ref={this.dialogRef}>
-					<Dialog.Header>Settings</Dialog.Header>
-					<Dialog.Body>
-						<div>
-							Enable dark theme <Switch checked={this.state.darkThemeEnabled} onClick={this.toggleDarkTheme} />
-						</div>
-					</Dialog.Body>
-					<Dialog.Footer>
-						<Dialog.FooterButton accept>OK</Dialog.FooterButton>
-					</Dialog.Footer>
-				</Dialog>
-			</div>
-		);
-	}
+  render(props) {
+    return (
+      <div>
+        <TopAppBar className="topappbar">
+          <TopAppBar.Row>
+            <TopAppBar.Section align-start>
+              <TopAppBar.Icon menu onClick={this.openDrawer}>
+                menu
+              </TopAppBar.Icon>
+              <TopAppBar.Title
+                style={{ cursor: 'pointer' }}
+                onClick={this.goHome}
+              >
+                <img class={style.smolIcon} src={smolThing} />
+                Agua+
+              </TopAppBar.Title>
+            </TopAppBar.Section>
+            <TopAppBar.Section align-end shrink-to-fit>
+              <SpaceMeter />
+              <TopAppBar.Icon onClick={this.openSettings}>
+                settings
+              </TopAppBar.Icon>
+            </TopAppBar.Section>
+          </TopAppBar.Row>
+        </TopAppBar>
+        <Drawer modal ref={this.drawerRef}>
+          <Drawer.DrawerContent>
+            <Drawer.DrawerItem
+              selected={props.selectedRoute === '/'}
+              onClick={this.goHome}
+            >
+              <List.ItemGraphic>home</List.ItemGraphic>
+              Home
+            </Drawer.DrawerItem>
+            <Drawer.DrawerItem
+              selected={props.selectedRoute === '/upload'}
+              onClick={this.goToUpload}
+            >
+              <List.ItemGraphic>cloud_upload</List.ItemGraphic>
+              Upload
+            </Drawer.DrawerItem>
+            <Drawer.DrawerItem
+              selected={props.selectedRoute === '/bugs'}
+              onClick={this.goToBugs}
+            >
+              <List.ItemGraphic>bug_report</List.ItemGraphic>
+              Bug Reporting
+            </Drawer.DrawerItem>
+            <Drawer.DrawerItem
+              selected={props.selectedRoute === '/profile'}
+              onClick={this.goToMyProfile}
+            >
+              <List.ItemGraphic>account_circle</List.ItemGraphic>
+              Profile
+            </Drawer.DrawerItem>
+          </Drawer.DrawerContent>
+        </Drawer>
+        <Dialog ref={this.dialogRef}>
+          <Dialog.Header>Settings</Dialog.Header>
+          <Dialog.Body>
+            <div>
+              Enable dark theme{' '}
+              <Switch
+                checked={this.state.darkThemeEnabled}
+                onClick={this.toggleDarkTheme}
+              />
+            </div>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Dialog.FooterButton accept>OK</Dialog.FooterButton>
+          </Dialog.Footer>
+        </Dialog>
+      </div>
+    );
+  }
 }
